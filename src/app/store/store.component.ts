77796ac4bd075081b8a65a5405c1cba941504a88
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-06-16 18:20:18 +0300

    Adding Category Selection

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,13 +7,20 @@ import { ProductRepository } from "../model/product.repository";
     templateUrl: 'store.component.html'
 })
 export class StoreComponent {
+
+    selectedCategory: string | undefined;
+
     constructor(private repository: ProductRepository) { }
     get products(): Product[] {
-        return this.repository.getProducts();
+        return this.repository.getProducts(this.selectedCategory);
     }
 
 
     get categories(): string[] {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
     }
 }
