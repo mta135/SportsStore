6fd7646cb2b5c6858eb7acc3e1c5c3450406e8f7
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2024-06-18 22:04:27 +0300

    Updating the Feature Module

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
+import { OrderRepository } from "./order.repository";
+import { Order } from "./order.model";
 
 
 @NgModule({
     providers: [
-        ProductRepository,
-        StaticDataSource,
-        Cart
+        ProductRepository, StaticDataSource, Cart, Order, OrderRepository
     ]
 })
 
